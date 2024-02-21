05a6b0ad2d4d294759755972b43cca8f1ff6aaf4
Khalid <dev.y2mi@gmail.com>
2024-02-21 13:47:39 +0300

    Delete legainaha_flutter_workshop_day3/drag_drop_game directory

--- legainaha_flutter_workshop_day3/drag_drop_game/util/data.dart
+++
@@ -1,9 +0,0 @@
-/// [score] used to calculate player score
-Map<String, bool> score = {};
-
-/// [buzzell] containes the choice and its value
-Map<String, String> buzzell = {
-  'assets/codelab.png': 'Center Of Degital Enterpreneurship',
-  'assets/legainaha.png': 'Unicorn 🦄💙',
-  'assets/m_Logo.png': 'S&M Enterprise General Authority',
-};
